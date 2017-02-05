1014c484ede63de7a80067ba1d5674f7489b96b4
hunter01 <is.it.fucking.needed@satan.org>
2017-02-05 14:56:18 +0000

    added some swagger docs

--- core/resources/status.py
+++ core/resources/status.py
@@ -3,6 +3,15 @@ from flask_restful_swagger import swagger
 
 class Status(Resource):
 
-	@swagger.operation()
+	@swagger.operation(
+		notes='A quick and easy test of whether the core server is live',
+		nickname='Server Status',
+		responseMessages=[
+			{
+				"code": 200,
+				"message": "Server is live"
+			}
+		]
+		)
 	def get(self):
 		return {'status':'live'}
