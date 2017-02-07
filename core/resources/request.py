41dbc73b6f791a50af8492d70c95983741a178be
hunter01 <is.it.fucking.needed@satan.org>
2017-02-07 23:34:37 +0000

    resource fields added

--- core/resources/request.py
+++ core/resources/request.py
@@ -1,5 +1,15 @@
-from flask_restful import Resource
+from flask_restful import fields, marshal_with, request, Resource
 from flask_restful_swagger import swagger
+
+@swagger.model
+class RequestModel:
+	resource_fields = {
+		'id': fields.String,
+		'created': fields.DateTime,
+		'status': fields.Boolean,
+		'action': fields.String
+	}
+
 
 class Request(Resource):
 
@@ -19,5 +29,36 @@ class Request(Resource):
 				'message': 'There are no requests to get'
 			}
 		])
+	@marshal_with(RequestModel.resource_fields)
 	def get(self):
-		pass
+		pass #jsonify data
+
+	@swagger.operation(
+		notes='Adds a request to the server',
+		nickname="Add request",
+		parameters=[
+		{
+			'name': 'body',
+			'description': 'request to be added',
+			'required': True,
+			'allowMultiple': True,
+			'dataType': RequestModel.__name__,
+			'paramType': 'body'
+		}
+		],
+		responseMessages=[
+			{
+				'code': 201,
+				'message': 'Request created'
+			},
+			{
+				'code': 405,
+				'message': 'Invalid request'
+			}
+		])
+	@marshal_with(RequestModel.resource_fields)
+	def post(self):
+		req = request.get_json()
+		print req
+
+
